639fef1a860df64528c7ff2f44ad556300d05dca
bijay_d <iambijaydulal@gmail.com>
2023-08-11 07:41:54 +0545

    Request Header Parser

--- index.js
+++ index.js
@@ -1,28 +1,34 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config();
-const multer = require('multer');
-const upload = multer();
+// index.js
+// where your node app starts
 
+// init project
+require('dotenv').config();
+var express = require('express');
 var app = express();
 
-app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC
+var cors = require('cors');
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
 app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+  res.sendFile(__dirname + '/views/index.html');
 });
 
-app.post("/api/fileanalyse",upload.single('upfile'),(req,res)=>{
+// your first API endpoint...
+app.get('/api/whoami', function (req, res) {
   res.json({
-    name: req.file.originalname,
-    type: req.file.mimetype,
-    size: req.file.size
+    "ipaddress": req.ip,
+    "language" : req.headers['accept-language'],
+    "software" : req.headers['user-agent']
   });
 });
 
-
-const port = process.env.PORT || 3000;
-app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+// listen for requests :)
+var listener = app.listen(process.env.PORT || 3000, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
 });
